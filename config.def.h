6a73f2b2df254dc442bf5267cbb0a8fd63e9247e
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2013-09-05 22:51:53 +0100

    Applied st-0.4.1-argbbg.diff patch.

--- config.def.h
+++ config.def.h
@@ -42,6 +42,8 @@ static char termname[] = "st-256color";
 
 static unsigned int tabspaces = 8;
 
+/* background opacity */
+static const int alpha = 0xdd;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -69,6 +71,7 @@ static const char *colorname[] = {
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
+	"black",
 };
 
 
@@ -77,7 +80,7 @@ static const char *colorname[] = {
  * foreground, background, cursor
  */
 static unsigned int defaultfg = 7;
-static unsigned int defaultbg = 0;
+static unsigned int defaultbg = 257;
 static unsigned int defaultcs = 256;
 
 /*
